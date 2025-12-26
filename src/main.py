0426097cfa9891581a04a77d3789f0c8c332a746
Kiarash1380mohebbi <Kiarashmohebbi00@gmail.com>
2025-12-26 08:27:36 -0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,9 +2,16 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.add(ft.Text(value="پیش بینی قیمت خانه"))
+    
+    page.appbar = ft.AppBar(
+    leading=ft.Icon(ft.Icons.MENU),
+    title=ft.Text("Dashboard"),
+    actions=[
+        ft.IconButton(ft.Icons.WB_SUNNY_OUTLINED)
+        ],
+        bgcolor=ft.Colors.SURFACE_CONTAINER,)
+    page.title = "Prediction Home App"
     page.window.maximized = True
+    page.add(ft.Text(value="پیش بینی قیمت خانه"))
 
-
-if __name__ == "__main__":
-    ft.run(main)
+ft.run(main)
