4dc35edad19eb959ff7929d506068e8b1b554855
Kiarash1380mohebbi <Kiarashmohebbi00@gmail.com>
2026-01-03 04:24:22 -0800

    ریسپانسیو کردن شی های اپ

--- src/main.py
+++ src/main.py
@@ -10,8 +10,14 @@ def main(page: ft.Page):
         ft.IconButton(ft.Icons.WB_SUNNY_OUTLINED)
         ],
         bgcolor=ft.Colors.SURFACE_CONTAINER,)
-    page.title = "Prediction Home App"
+    page.title = "Home Price App"
     page.window.maximized = True
-    page.add(ft.Text(value="پیش بینی قیمت خانه"))
+    
 
+    page.add(ft.ResponsiveRow(ft.Column(col={"sm": 6},controls=[ft.TextField(label="Search...",),
+                 ft.ElevatedButton(icon=ft.Icons.SEARCH)]),rtl=True))
+    page.add(ft.ResponsiveRow([ft.Column(col={"sm": 6}, controls=[ft.Text("نمودار قیمت خانه های تهران")]),
+                     ft.Column(col={"sm": 6}, controls=[ft.Text("لیست قیمت خانه ها")])],rtl=True))
+
+    
 ft.run(main)
